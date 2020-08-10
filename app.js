b0f043ad8d787ea37bf4d1241655e0e5707a6dd2
RebeccaHough <rebecca.c.hough@blueyonder.co.uk>
2020-08-10 15:22:23 +0100

    remove: ejs; add: users endpoints

--- app.js
+++ app.js
@@ -4,20 +4,26 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// mongoDB
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/ready-set-goal-backend-mongodb');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// make db accessible to  router
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -33,9 +39,9 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // send error
   res.status(err.status || 500);
-  res.render('error');
+  res.json(err);
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,60 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+  res.send('');
+});
+
+/* GET userlist page */
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({}, {}, function(e, docs) {
+    res.json({"userlist": docs})
+  })
+});
+
+/* POST to sign up service*/
+router.post('/adduser', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+
+  var username = req.body.username;
+  var email = req.body.email;
+
+  //validation
+  //TODO
+  //validate email syntax
+  //disallow duplicate emails/usernames
+  if(username && email) {
+    //submit to db
+    // collection.insert(req.body, function(err, result){
+    //   res.send(
+    //     (err === null) ? { msg: '' } : { msg: err }
+    //   );
+    // });
+    collection.insert({
+      'username': username,
+      'email': email
+    }, function(err, doc) {
+      res.json(err || '');
+    })
+  } else {
+    res.json('Malformed');
+  }
+
+});
+
+/* DELETE user */
+router.delete('/deleteuser/:id', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+
+  var userToDelete = req.params.id;
+
+  collection.remove({ '_id' : userToDelete }, function(err) {
+    res.send((err === null) ? { msg: '' } : { msg: {'error': err }});
+  });
 });
 
 module.exports = router;
